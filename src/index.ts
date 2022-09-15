9c507a30bd89f7709ed75ccc268eed29d3c23a9b
nawazmoin <syedmoin.nawaz@yahoo.com>
2022-09-15 11:31:55 +0200

    index.ts changes

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { registerPlugin } from '@capacitor/core';
 
-import type { OAuth2ClientPlugin } from './definitions';
+import type { OAuth2AuthenticateOptions, OAuth2ClientPlugin } from './definitions';
 
 const OAuth2Client = registerPlugin<OAuth2ClientPlugin>('OAuth2Client', {
     web: () => import('./web').then(m => new m.OAuth2ClientPluginWeb()),
@@ -8,4 +8,22 @@ const OAuth2Client = registerPlugin<OAuth2ClientPlugin>('OAuth2Client', {
 });
 
 export * from './definitions';
-export { OAuth2Client };
+
+
+const getAccessToken=async(settings:OAuth2AuthenticateOptions)=>{
+
+    const response = await OAuth2Client.authenticate(settings);
+    const accessToken = response['access_token_response']['access_token'];
+    return accessToken;
+
+}
+
+
+
+// export const refreshAccessToken=async(settings:OAuth2AuthenticateOptions)=>{
+//     const response = await OAuth2Client.refreshToken(settings);
+//     const accessToken = response['access_token_response']['access_token'];
+//     return accessToken;
+// }
+
+export { OAuth2Client, getAccessToken };
